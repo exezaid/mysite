b1557e378f18dda5af8f47bd11f8092a4b0247ee
Ezequiel <ezequiel@ezequiel-desktop.(none)>
2010-05-01 17:49:23 -0300

    2 edit

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from mysite.views import current_datetime
+from mysite.views import hours_ahead 
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,4 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
     (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
--- views.py
+++ views.py
@@ -6,3 +6,8 @@ def current_datetime(request):
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
 
+def hours_ahead(request, offset):
+    offset = int(offset)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
