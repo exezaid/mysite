4f2396e83233bd7416ae44141b1a3c24065b78ea
Ezequiel <ezequiel@ezequiel-desktop.(none)>
2010-05-02 19:49:22 -0300

    edit view

--- books/views.py
+++ books/views.py
@@ -6,10 +6,14 @@ def search_form(request):
     return render_to_response('search_form.html')
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
-		q = request.GET['q']
-		books = Book.objects.filter(title__icontains=q)
-		return render_to_response('search_results.html',
-		    {'books': books, 'query': q})
-    else:
-        return render_to_response('search_form.html', {'error': True})
+    error = False
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html',
+                {'books': books, 'query': q})
+    return render_to_response('search_form.html',
+	    {'error': error})
