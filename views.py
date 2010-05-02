9844c926ab5a47760e5d823c809db2be5a4d679b
Ezequiel <ezequiel@ezequiel-desktop.(none)>
2010-05-01 21:55:22 -0300

    extends base.html

--- views.py
+++ views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render_to_response
 import datetime
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    return render_to_response('current_datetime.html', {'current_date': now})) 
+    current_date = datetime.datetime.now()
+    return render_to_response('current_datetime.html', locals()) 
 
 def hours_ahead(request, offset):
-    offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    hour_offset = int(offset)
+    next_time = datetime.datetime.now() + datetime.timedelta(hours=hour_offset)
+    return render_to_response('hours_ahead.html', locals())
